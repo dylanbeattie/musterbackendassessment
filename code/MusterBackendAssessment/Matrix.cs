644a4edf3a191a3ebda14d36ee1b8480750f3b97
dylanbeattie <dylan@dylanbeattie.net>
2022-06-27 13:40:54 +0100

    Test fixtures

--- code/MusterBackendAssessment/Matrix.cs
+++ code/MusterBackendAssessment/Matrix.cs
@@ -9,14 +9,20 @@ public class Matrix {
     ///<summary>Create a matrix by parsing a string containing comma-separated elements
     /// on rows separated by newlines</summary>
     public static Matrix Parse(string input) {    
-        return new Matrix(new int[,] { { 1,2,3}, {4,5,6}, {7,8,9} });
+        // TODO: instead of hard-coding the matrix,
+        // you need to split the string input into lines,
+        // split each line into tokens (numbers), parse each number
+        // into an integer, and then construct a new matrix using
+        // that set of numbers.
+        return new Matrix(new int[,] { { 1,1}, {2,2} });
     }
 
     public Matrix Transpose() {
-        return new Matrix(new int[,] { { 1,2,3}, {4,5,6}, {7,8,9} });
+        // TODO: Return a new matrix whose contents
+        // are the transpose of the current matrix.
+        return new Matrix(new int[,] { { 1,1,1}, {2,2,2}, {3,3,3} });
     }
-
     public override string ToString() {
-        return "//todo: fix this!";
+        return "//todo: return this matrix as a string :)";
     }
 }
