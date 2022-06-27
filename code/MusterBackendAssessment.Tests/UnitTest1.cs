8c6ed1b70ac5cc996376d5f34171c0941f1582ae
dylanbeattie <dylan@dylanbeattie.net>
2022-06-27 13:38:16 +0100

    Working examples for dotnet test

--- code/MusterBackendAssessment.Tests/UnitTest1.cs
+++ code/MusterBackendAssessment.Tests/UnitTest1.cs
@@ -1,12 +1,36 @@
 using Xunit;
+using MusterBackendAssessment;
+using Shouldly;
 
 namespace MusterBackendAssessment.Tests;
 
-public class UnitTest1
-{
+public class MatrixTests {
     [Fact]
-    public void Test1()
-    {
+    public void Parsing_Matrix_Works() {
+        var matrix = Matrix.Parse(@"1,2,3
+4,5,6
+7,8,9");
+        var expected = new int[,] { { 1,2,3}, {4,5,6}, {7,8,9}};        
+        matrix.Contents.ShouldBe(expected);
+    }
 
+    [Fact]
+    public void Transpose_Matrix_Works() {
+        var matrix = Matrix.Parse(@"1,2,3
+4,5,6
+7,8,9");
+        var transpose = matrix.Transpose();
+        var expected = new int[,] { { 9,8,7}, {6,5,4}, {3,2,1}};
+        transpose.Contents.ShouldBe(expected);
+    }
+
+    [Fact]
+    public void Matrix_ToString_Works() {
+        var input = @"1,2,3
+4,5,6
+7,9,9";
+        var matrix = Matrix.Parse(input);
+        var output = matrix.ToString();
+        output.ShouldBe(input);
     }
 }
--- code/MusterBackendAssessment/Matrix.cs
+++ code/MusterBackendAssessment/Matrix.cs
@@ -1,18 +1,22 @@
 using System;
 
 public class Matrix {
-    private int[,] contents;
+    private Matrix(int[,] contents) {
+        this.Contents = contents;
+    }
+    public int[,] Contents { get; init; }
+    
     ///<summary>Create a matrix by parsing a string containing comma-separated elements
     /// on rows separated by newlines</summary>
-    public static Matrix Parse(string input) {
-        throw new NotImplementedException();
+    public static Matrix Parse(string input) {    
+        return new Matrix(new int[,] { { 1,2,3}, {4,5,6}, {7,8,9} });
     }
 
-    public Matrix Invert() {
-        throw new NotImplementedException();
+    public Matrix Transpose() {
+        return new Matrix(new int[,] { { 1,2,3}, {4,5,6}, {7,8,9} });
     }
 
     public override string ToString() {
-        throw new NotImplementedException();
+        return "//todo: fix this!";
     }
 }
